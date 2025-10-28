4649ddd864a9bd451bd819cd44c71a1032e244c0
Akshata Deole <deoleakshata@gmail.com>
2025-10-28 17:54:14 +0530

    Assignment_19 Solution(Q1 - Q5)

---
+++ Assignments/Assignment_19/program19_1.java
@@ -0,0 +1,35 @@
+///////////////////////////////////////////////////////////////
+// Function Name : checkLeapYear
+// Description   : Check whether a given year is a leap year or
+//                 not.
+// Input         : Integer
+// Output        : String 
+// Author        : Akshata Dhananjay Deole
+// Date          : 28/10/2025
+///////////////////////////////////////////////////////////////
+class Logic
+{
+    void checkLeapYear(int year)
+    {
+        if((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
+        {
+            System.out.println(year + "is a leap year");
+        }
+        else
+        {
+             System.out.println(year + "is not a leap year");
+        }
+        
+
+        
+    }
+}
+class program19_1
+{
+    public static void main(String args [])
+    {
+        Logic obj = new Logic();
+        obj.checkLeapYear(2024);
+        
+    }
+}
