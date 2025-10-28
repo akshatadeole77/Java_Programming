2893e2a8193b77f01b0677b9df0c3684dd97d698
Akshata Deole <deoleakshata@gmail.com>
2025-10-28 13:53:12 +0530

    Assignment_16 solution(Q1-Q5)

---
+++ Assignments/Assignment_16/program16_3.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////
+// Function Name : findFactorial
+// Description   : Accept number from user and find its factorial 
+//                 using a for loop.
+// Input         : Integer
+// Output        : Integer 
+// Author        : Akshata Dhananjay Deole
+// Date          : 28/10/2025
+///////////////////////////////////////////////////////////////
+
+
+
+class Logic
+{
+    void findFactorial(int num)
+    {
+       int fact = 1;
+
+       for(int i = 1; i <= num; i++)
+       {
+        fact = fact * 1;
+       }
+       System.out.println("Factorial of" +num+ "is:"+fact);
+    }
+}
+class program16_3
+{
+    public static void main (String args[])
+    {
+        Logic obj = new Logic();
+        obj.findFactorial(5);
+    }
+}
