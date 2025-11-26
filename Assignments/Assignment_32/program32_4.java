b23fa0934e8502dd2ebcbd977988bcff312f062e
Akshata Deole <deoleakshata@gmail.com>
2025-11-27 00:18:01 +0530

    Add files via upload

---
+++ Assignments/Assignment_32/program32_4.java
@@ -0,0 +1,62 @@
+
+
+import java.util.Scanner;
+
+class Pattern
+{
+    
+    public void Display(int iRow, int iCol)
+    {
+       int i = 0, j = 0;
+
+       if(iRow != iCol)
+       {
+        System.out.println("Invalid input");
+        System.out.println("Row number and column number should be same");
+       }
+       
+       
+        for(i=1;i<=iRow;i++)
+        {
+            
+            for(j=iCol;j>=1;j--)
+            {
+                if(j == i||i == 1||i == iRow||j == 1||j == iCol)
+                {
+                    System.out.print("*\t");  
+                }
+                else if(j > i)
+                {
+                    System.out.println("#\t");
+                }
+                else
+                {
+                    System.out.println("$\t");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+class program32_3
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter the numbers of rows :");
+        iValue1 = sobj.nextInt();
+
+        
+        System.out.println("Enter the numbers of columns :");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1, iValue2);
+        
+    }
+}
